45ef2051483d2b2f5d6bd778fc648be31ca9c00e
AkoviMensah <abmensah@wisc.edu>
2023-01-02 17:51:25 -0500

    Added MediaDbContext and to the services

--- API/Data/MediaContext.cs
+++ API/Data/MediaContext.cs
@@ -1,6 +1,12 @@
-﻿namespace API.Data
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
 {
-    public class MediaContext
+    public class MediaContext: DbContext
     {
+        public MediaContext(DbContextOptions<MediaContext> options): base(options) { }
+
+        public DbSet<Song> Songs { get; set; }
     }
 }
